f833da4b8c259d4c4a2df4245d0c8e0500e947b9
Eriene Galinato <erienegalinato@gmail.com>
2025-03-17 12:56:03 +0800

    feat: replace `adapter-static` with `sveltekit-websocket`

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
-/*
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@ubermanu/sveltekit-websocket';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
- @type {import('@sveltejs/kit').Config} 
+/** @type {import('@sveltejs/kit').Config} */ 
 const config = {
     // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
@@ -16,14 +15,3 @@ const config = {
 };
 
 export default config;
-*/
-
-import adapter from '@ubermanu/sveltekit-websocket'
-
-const config = {
-  kit: {
-    adapter: adapter(),
-  },
-}
-
-export default config
