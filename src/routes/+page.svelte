713a754f920852f64f0ca0806507ee6caf0f4c8a
Eriene Galinato <erienegalinato@gmail.com>
2025-03-19 00:13:47 +0800

    feat: revert previous `+page.svelte`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<script>
-    import { browser } from '$app/environment'
-    import { page } from '$app/state'
-  
-    const socket = browser ? new WebSocket(`ws://buhay-backend-production.up.railway.app/ws/123`) : null
-    
-    let connected = $state(false)
-    let buffer = $state([])
-    socket?.addEventListener('open', () => (connected = true))
-    socket?.addEventListener('close', () => (connected = false))
-    socket?.addEventListener('message', (message) => (buffer.push(message.data)))
-
-    
-    
-  </script>
-  
-  <p>
-    Websocket connection status: {connected ? '🟢' : '🔴'}
-    Websocket entries: 
-  
-  </p>
-  <ul>
-    {#each buffer as entry}
-    <li>{entry}</li>
-    {/each}
-  </ul>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
