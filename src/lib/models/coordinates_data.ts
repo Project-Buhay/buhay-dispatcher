c7e5e83189d58b9c639a8fd000e35704256a0f30
Eriene Galinato <erienegalinato@gmail.com>
2025-03-19 00:29:25 +0800

    feat: add `CoordinatesData` schema

---
+++ src/lib/models/coordinates_data.ts
@@ -0,0 +1,15 @@
+import { array, object, type InferOutput, pipe, transform, string } from 'valibot';
+
+export const CoordinatesDatum = object({
+    data: pipe(string(), transform((input) => {
+        const arr = input.split(',');
+        let nums: number[] = [];
+        arr.forEach(st => { nums.push(Number(st)); });
+        return nums;
+    })),
+});
+
+export const CoordinatesData = array(CoordinatesDatum);
+
+export type CoordinatesDatum = InferOutput<typeof CoordinatesDatum>;
+export type CoordinatesData = InferOutput<typeof CoordinatesData>;
