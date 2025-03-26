14574be2f45305a505a54b14c6a440aad9321f1e
Eriene Galinato <erienegalinato@gmail.com>
2025-03-26 23:23:33 +0800

    feat(dashboard): redirect to `/login` on invalid access

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,16 @@
+import { redirect } from '@sveltejs/kit';
 import { parse } from 'valibot';
 
 import { PUBLIC_API_URL } from '$env/static/public';
 import { Rescuers as RescuersSchema } from '$lib/models/rescuers.js';
 
-export async function load({ fetch }) {
+export async function load({ cookies, fetch }) {
+    const access_control = cookies.get('sessionid');
+
+    if (access_control !== '3') {
+        redirect(307, '/login');
+    }
+
     const response = await fetch(`http://${PUBLIC_API_URL}/get_rescuers`, { method: 'POST' });
     const raw_json = await response.json();
     const rescuers = parse(RescuersSchema, raw_json.rescuers);
