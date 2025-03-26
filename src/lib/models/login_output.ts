401a796d3aa920c341d61af55df6cd7151884649
Eriene Galinato <erienegalinato@gmail.com>
2025-03-26 16:36:11 +0800

    feat: add `LoginOutput` schema

---
+++ src/lib/models/login_output.ts
@@ -0,0 +1,8 @@
+import { number, object, type InferOutput } from 'valibot'
+
+export const LoginOutput = object({
+    person_id: number(),
+    access_control: number()
+});
+
+export type LoginOutput = InferOutput<typeof LoginOutput>;
