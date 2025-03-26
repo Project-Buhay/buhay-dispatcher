74c7b8a266e4370b1b4b7efff02229bf5c468720
Eriene Galinato <erienegalinato@gmail.com>
2025-03-26 08:51:04 +0800

    fix: parse `dispatcher_data` in `AssignmentPanel`

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,7 +2,7 @@
     import AssignmentPanel from '$lib/components/panels/AssignmentPanel.svelte';
 
     let { data } = $props();
-    let { dispatcher_data, rescuers } = $derived(data);
+    let { rescuers } = $derived(data);
 </script>
 
-<AssignmentPanel {dispatcher_data} {rescuers}></AssignmentPanel>
+<AssignmentPanel {rescuers}></AssignmentPanel>
