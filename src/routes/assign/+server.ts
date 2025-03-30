50846f4587e368fccb9bedb6ed253e15d393c7b1
Eriene Galinato <erienegalinato@gmail.com>
2025-03-30 12:04:51 +0800

    feat: add `/assign` proxy endpoint

---
+++ src/routes/assign/+server.ts
@@ -0,0 +1,24 @@
+import { json } from '@sveltejs/kit';
+
+import { PUBLIC_API_URL } from '$env/static/public';
+
+export async function GET({ fetch, url }) {
+    const request_id = url.searchParams.get('request_id');
+    const rescuer_id = url.searchParams.get('rescuer_id');
+
+    const updated = await fetch(`http://${PUBLIC_API_URL}/assign`, {
+        method: 'POST',
+        body: JSON.stringify({
+            request_id: request_id,
+            rescuer_id: rescuer_id
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+
+    const res = await updated.json()
+
+    console.log(res);
+    return json(updated);
+}
