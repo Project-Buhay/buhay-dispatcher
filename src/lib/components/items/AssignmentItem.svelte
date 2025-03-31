c8e29835e90c78a61c0b74e73767c91e5316728f
Eriene Galinato <erienegalinato@gmail.com>
2025-03-31 09:35:52 +0800

    chore: run formatters and linters

--- src/lib/components/items/AssignmentItem.svelte
+++ src/lib/components/items/AssignmentItem.svelte
@@ -35,74 +35,73 @@
 </script>
 
 {#if !reassigned}
-<div
-    class="my-10 flex w-5/6 content-center rounded-xl bg-blue-neutral p-4 text-white sm:mx-8 md:mx-16 lg:mx-36"
-    in:fly|global={{ x: -100 }} out:fly={{ x: 100 }}
->
-    <!-- Request ID -->
-    <div class="mr-10 w-1/6 flex-initial content-center text-2xl font-bold">
-        <p class="h-8 text-center align-middle">{request_id}</p>
-    </div>
+    <div
+        class="my-10 flex w-5/6 content-center rounded-xl bg-blue-neutral p-4 text-white sm:mx-8 md:mx-16 lg:mx-36"
+        in:fly|global={{ x: -100 }}
+        out:fly={{ x: 100 }}
+    >
+        <!-- Request ID -->
+        <div class="mr-10 w-1/6 flex-initial content-center text-2xl font-bold">
+            <p class="h-8 text-center align-middle">{request_id}</p>
+        </div>
 
-    <!-- Collapsible Location Names -->
-    <div class="mr-10 w-1/3 flex-initial content-center">
-        <button class="flex items-center text-xl font-bold" onclick={displayLocs}>
-            Locations
-            {#if are_locs_displayed}
-                <ChevronUpCircle class="ml-2" />
-            {:else}
-                <ChevronDownCircle class="ml-2" />
-            {/if}
-        </button>
-        {#each location_names as loc}
-            <div class="{are_locs_displayed ? 'block' : 'hidden'} mt-2">{loc}</div>
-        {/each}
-    </div>
-
-    <!-- Rescuer Assignment -->
-    <div class="mr-10 w-1/6 content-center">
-        <select
-            name="ass_rescuer"
-            bind:value={selected_rescuer_id}
-            class="ml-[42px] w-full rounded-xl bg-white text-black"
-        >
-            <option class="bg-black text-white" value={null}>Choose</option>
-            {#each rescuers as { person_id, username }}
-                <option class="bg-black text-white" value={person_id}>{username}</option>
+        <!-- Collapsible Location Names -->
+        <div class="mr-10 w-1/3 flex-initial content-center">
+            <button class="flex items-center text-xl font-bold" onclick={displayLocs}>
+                Locations
+                {#if are_locs_displayed}
+                    <ChevronUpCircle class="ml-2" />
+                {:else}
+                    <ChevronDownCircle class="ml-2" />
+                {/if}
+            </button>
+            {#each location_names as loc}
+                <div class="{are_locs_displayed ? 'block' : 'hidden'} mt-2">{loc}</div>
             {/each}
-        </select>
-    </div>
+        </div>
 
-    <!-- Assign Button -->
-    <div class="ml-30 flex-initial basis-1/6 content-center justify-center">
-        {#if !(rescuer_id === null)}
-            {#if selected_rescuer_id === rescuer_id}
-                <button
-                    class="ml-[42px] h-16 w-[125px] flex-initial rounded-2xl bg-[#144359] p-4 font-bold text-white"
-                    disabled>Assigned</button
-                >
+        <!-- Rescuer Assignment -->
+        <div class="mr-10 w-1/6 content-center">
+            <select
+                name="ass_rescuer"
+                bind:value={selected_rescuer_id}
+                class="ml-[42px] w-full rounded-xl bg-white text-black"
+            >
+                <option class="bg-black text-white" value={null}>Choose</option>
+                {#each rescuers as { person_id, username }}
+                    <option class="bg-black text-white" value={person_id}>{username}</option>
+                {/each}
+            </select>
+        </div>
+
+        <!-- Assign Button -->
+        <div class="ml-30 flex-initial basis-1/6 content-center justify-center">
+            {#if !(rescuer_id === null)}
+                {#if selected_rescuer_id === rescuer_id}
+                    <button
+                        class="ml-[42px] h-16 w-[125px] flex-initial rounded-2xl bg-[#144359] p-4 font-bold text-white"
+                        disabled>Assigned</button
+                    >
+                {:else}
+                    <button
+                        class="ml-[42px] h-16 w-[125px] flex-initial rounded-2xl p-4 font-semibold {selected_rescuer_id ===
+                        null
+                            ? 'text-white/50'
+                            : 'hover:bg-white hover:text-black hover:duration-150'}"
+                        onclick={assign}
+                        disabled={selected_rescuer_id === null}>Reassign</button
+                    >
+                {/if}
             {:else}
                 <button
-                    class="ml-[42px] h-16 w-[125px] flex-initial rounded-2xl p-4 font-semibold {selected_rescuer_id ===
-                    null
+                    class="ml-[42px] h-16 w-[125px] flex-initial rounded-2xl p-4
+            font-semibold {selected_rescuer_id === null
                         ? 'text-white/50'
                         : 'hover:bg-white hover:text-black hover:duration-150'}"
                     onclick={assign}
-                    disabled={selected_rescuer_id === null}>Reassign</button
+                    disabled={selected_rescuer_id === null}>Assign</button
                 >
             {/if}
-        {:else}
-            <button
-                class="ml-[42px] h-16 w-[125px] flex-initial rounded-2xl p-4
-        font-semibold {selected_rescuer_id === null
-                    ? 'text-white/50'
-                    : 'hover:bg-white hover:text-black hover:duration-150'}"
-                onclick={assign}
-                disabled={selected_rescuer_id === null}>Assign</button
-            >
-        {/if}
+        </div>
     </div>
-
-    <!-- Calculate TSP (if needed)-->
-</div>
 {/if}
